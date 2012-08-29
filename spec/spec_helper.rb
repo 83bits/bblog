cd772f2b1dda0c175c93f467ae03ab18cc3a722b
David Leung <david@davidslab.com>
2012-08-29 13:26:22 +0800

    Replaced Test::Unit with RSpec + Capybara

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,14 +2,15 @@
 ENV["RAILS_ENV"] = "test"
 
 require File.expand_path("../dummy/config/environment.rb",  __FILE__)
-require "rails/test_help"
+require "rspec/rails"
 
 Rails.backtrace_cleaner.remove_silencers!
 
 # Load support files
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 
-# Load fixtures from the engine
-if ActiveSupport::TestCase.method_defined?(:fixture_path=)
-  ActiveSupport::TestCase.fixture_path = File.expand_path("../fixtures", __FILE__)
+RSpec.configure do |config|
+  config.before(:each) do
+    Mongoid::Sessions.default.collections.select {|c| c.name !~ /system/ }.each(&:drop)
+  end
 end
--- test/bblog_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class BblogTest < ActiveSupport::TestCase
-  test "truth" do
-    assert_kind_of Module, Bblog
-  end
-end
